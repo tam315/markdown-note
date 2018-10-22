adf9783ed54b714a6aec8b1623f3a02521e9d981
stam <junkboy0315@gmail.com>
2018-10-22 10:51:43 +0900

    adopt Lighthouse

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#2E6BE6' }],
   ],
   themeConfig: {
     sidebar: 'auto',
