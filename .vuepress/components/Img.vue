6de2ebaba69096b372e9c92647ab88c0ac51d09d
Shota Tamura <tamura@yuuniworks.com>
2019-05-24 17:59:10 +0900

    add component to show images

---
+++ .vuepress/components/Img.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :style="styles.container">
+    <div v-if="link">
+      <a :href="link" target="_blank">
+        <img :src="src">
+      </a>
+    </div>
+    <div v-else>
+      <img :src="src">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["maxWidth", "src", "link"],
+  data() {
+    return {
+      styles: {
+        container: {
+          margin: "1rem 0",
+          maxWidth: this.maxWidth ? this.maxWidth : null
+        }
+      }
+    };
+  }
+};
+</script>
+
